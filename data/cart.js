1b393a8aefc5641a2780528eb5224c6c099d285c
Michael Mesfin <michael.21duke@gmail.com>
2026-01-06 03:20:04 -0500

    Extend cart to an Object, and add total to calculate total quantity

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,31 @@
-export const cart = [];
+export const cart = {
+  items: [
+    {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 1,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 2,
+    },
+  ],
+};
+
+cart.total = cart.items.reduce(
+  (total, cartItem) => total + cartItem.quantity,
+  0
+);
 
 export function addToCart(productId, selectedQuantity) {
   let matchingItem;
 
-  cart.forEach((cartItem) => {
+  cart.items.forEach((cartItem) => {
     if (cartItem.productId === productId) matchingItem = cartItem;
   });
 
   if (matchingItem) matchingItem.quantity += selectedQuantity;
   else
-    cart.push({
+    cart.items.push({
       productId,
       quantity: selectedQuantity,
     });
@@ -18,13 +34,8 @@ export function addToCart(productId, selectedQuantity) {
 }
 
 export function updateCartQuantity() {
-  const cartQuantity = cart.reduce(
-    (total, cartItem) => (total += cartItem.quantity),
-    0
-  );
-
   // Update the Cart quantity notificatioon
-  document.querySelector(".cart-quantity").innerHTML = cartQuantity;
+  document.querySelector(".cart-quantity").innerHTML = cart.total;
 }
 
 export default cart;
