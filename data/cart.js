3e9e90b3a5210b2a7c98bf1374e8cfc2d86738c6
Michael Mesfin <michael.21duke@gmail.com>
2026-01-08 21:34:57 -0500

    Save and persist the cart

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,19 @@
-export const cart = {
-  items: [
-    {
-      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-      quantity: 1,
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 2,
-    },
-  ],
-  total: 0,
-};
+export let cart = JSON.parse(localStorage.getItem("cart"));
+console.log(cart.items)
+if (!cart)
+  cart = {
+    items: [
+      {
+        productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        quantity: 1,
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2,
+      },
+    ],
+    total: 0,
+  };
 
 export function addToCart(productId, selectedQuantity) {
   let matchingItem;
@@ -26,6 +29,7 @@ export function addToCart(productId, selectedQuantity) {
       quantity: selectedQuantity,
     });
 
+  saveToStorage();
   console.log(cart);
 }
 
@@ -41,6 +45,11 @@ export function updateCartQuantity() {
 
 export function removeFromCart(productId) {
   cart.items = cart.items.filter((item) => item.productId !== productId);
+  saveToStorage();
+}
+
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 export default cart;
