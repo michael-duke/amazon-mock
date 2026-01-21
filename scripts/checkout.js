e8554b767a25fe9f5ecde4facf2167bdd9594662
Michael Mesfin <michael.21duke@gmail.com>
2026-01-21 16:31:02 -0500

    Implement the dynamic generation of delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,20 +4,20 @@ import cart, {
   saveToStorage,
   updateQuantity,
 } from "../data/cart.js";
+import deliverOptions from "../data/deliveryOptions.js";
 import products from "../data/products.js";
 import formatCurrency from "./utils/money.js";
-import dayjs from "https://unpkg.com/dayjs@1.11.19/esm/index.js";
+import formatDate from "./utils/date.js";
 
 updateCartQuantity();
 console.log(cart);
-console.log(dayjs().format('D'))
 
-cart.items.forEach(({ productId, quantity }) => {
+cart.items.forEach((item) => {
   const cartItemContainer = document.createElement("div");
   cartItemContainer.classList.add("cart-item-container");
-  cartItemContainer.classList.add(`cart-item-container-${productId}`);
+  cartItemContainer.classList.add(`cart-item-container-${item.productId}`);
 
-  const cartItem = products.find((product) => product.id === productId);
+  const cartItem = products.find((product) => product.id === item.productId);
 
   cartItemContainer.innerHTML = `
     <div class="delivery-date">
@@ -36,65 +36,54 @@ cart.items.forEach(({ productId, quantity }) => {
         </div>
         <div class="product-price">$${formatCurrency(cartItem.priceCents)}</div>
         <div class="product-quantity">
-            <span> Quantity: <span class="quantity-label">${quantity}</span> </span>
-            <span class="update-quantity-link link-primary" data-product-id="${productId}">
+            <span> Quantity: <span class="quantity-label">${item.quantity}</span> </span>
+            <span class="update-quantity-link link-primary" data-product-id="${item.productId}">
             Update
             </span>
-            <input class="quantity-input quantity-input-${productId}">
-            <span class="save-quantity-link link-primary" data-product-id="${productId}">
+            <input class="quantity-input quantity-input-${item.productId}">
+            <span class="save-quantity-link link-primary" data-product-id="${item.productId}">
             Save
             </span>
-            <span class="delete-quantity-link link-primary" data-product-id="${productId}">
+            <span class="delete-quantity-link link-primary" data-product-id="${item.productId}">
             Delete
             </span>
         </div>
         </div>
 
         <div class="delivery-options">
-        <div class="delivery-options-title">
-            Choose a delivery option:
-        </div>
-        <div class="delivery-option">
-            <input
-            type="radio"
-            checked
-            class="delivery-option-input"
-            name="delivery-option-${cartItem.id}"
-            />
-            <div>
-            <div class="delivery-option-date">Tuesday, June 21</div>
-            <div class="delivery-option-price">FREE Shipping</div>
-            </div>
-        </div>
-        <div class="delivery-option">
-            <input
-            type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${cartItem.id}"
-            />
-            <div>
-            <div class="delivery-option-date">Wednesday, June 15</div>
-            <div class="delivery-option-price">$4.99 - Shipping</div>
-            </div>
-        </div>
-        <div class="delivery-option">
-            <input
-            type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${cartItem.id}"
-            />
-            <div>
-            <div class="delivery-option-date">Monday, June 13</div>
-            <div class="delivery-option-price">$9.99 - Shipping</div>
-            </div>
-        </div>
+          <div class="delivery-options-title">
+              Choose a delivery option:
+          </div>
+          ${generateDeliveryOptions(item)}     
         </div>
     </div>       
     `;
-
   document.querySelector(".order-summary").appendChild(cartItemContainer);
 });
 
+function generateDeliveryOptions(item) {
+  console.log(item)
+  let deliveryOptionsHTML = "";
+  deliverOptions.forEach((option) => {
+    const isChecked = item.deliveryOptionId === option.id
+    deliveryOptionsHTML += `
+         <div class="delivery-option">
+              <input
+              type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${item.productId}"
+              ${isChecked ? 'checked': ''}
+              />
+              <div>
+              <div class="delivery-option-date">${formatDate(option.deliveryDays)}</div>
+              <div class="delivery-option-price">${option.priceCents === 0 ? "FREE" : "$" + formatCurrency(option.priceCents)} - Shipping</div>
+              </div>
+          </div>
+      `;
+  });
+
+  return deliveryOptionsHTML;
+}
 function updateCartQuantity() {
   // Calculate the Cart quantity
   const total = calculateTotalQuantity();
