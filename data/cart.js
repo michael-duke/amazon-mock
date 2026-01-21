d68588e85a0d3c28ba782343151d24b7e2c781f3
Michael Mesfin <michael.21duke@gmail.com>
2026-01-21 16:29:32 -0500

    Add a third key to cart deliveryOptionId Define and export deliveryOptions

--- data/cart.js
+++ data/cart.js
@@ -6,10 +6,12 @@ if (!cart)
       {
         productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
         quantity: 1,
+        deliveryOptionId: "1",
       },
       {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 2,
+        deliveryOptionId: "2",
       },
     ],
     total: 0,
@@ -27,6 +29,7 @@ export function addToCart(productId, selectedQuantity) {
     cart.items.push({
       productId,
       quantity: selectedQuantity,
+      deliveryOptionId: "1"
     });
 }
 
