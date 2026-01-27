181d7770fe8b17af0c6536b5d0487d9834785eea
Michael Mesfin <michael.21duke@gmail.com>
2026-01-27 14:13:51 -0500

    Change the Url to URL in getStarsURL

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product) => {
 
         <div class="product-rating-container">
           <img class="product-rating-stars"
-            src=${product.getStarsUrl()}>
+            src=${product.getStarsURL()}>
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
