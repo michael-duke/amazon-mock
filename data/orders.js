4388d7e246bb148b3e402a8bfcbdfb386f26d3d6
Michael Mesfin <michael.21duke@gmail.com>
2026-02-08 15:40:23 -0500

    Implement getOrder and placeOrder

--- data/orders.js
+++ data/orders.js
@@ -5,6 +5,28 @@ export function addOrder(order) {
   saveToStorage();
 }
 
+export function getOrder(orderId) {
+  return orders.find((order) => order.id === orderId);
+}
+
+export async function placeOrder(cart) {
+  try {
+    const response = await fetch("https://supersimplebackend.dev/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cart),
+    });
+
+    if (!response.ok) throw new Error("Order failed. Please try again");
+    const order = await response.json();
+    return order;
+  } catch (error) {
+    console.log("Error placing order: " + error);
+  }
+}
+
 function saveToStorage() {
   localStorage.setItem("orders", JSON.stringify(orders));
 }
