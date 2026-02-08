d761ab0fccd4e80aa26662406a976ad510d5ed81
Michael Mesfin <michael.21duke@gmail.com>
2026-02-08 01:12:50 -0500

    Remove lesson 17 from the amazon project

--- lesson 17j/scripts/utils/money.js
+++
@@ -1,9 +0,0 @@
-function formatCurrency(priceCents) {
-  return (Math.round(priceCents) / 100).toFixed(2);
-}
-
-export function formatTaxCent(priceCents) {
-  return (priceCents * 10) / 100;
-}
-
-export default formatCurrency;
