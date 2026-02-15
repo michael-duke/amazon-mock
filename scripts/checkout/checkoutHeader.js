bd896e43f150b20733b70da71d4a9671f90ea59a
Michael Mesfin <michael.21duke@gmail.com>
2026-02-14 21:33:22 -0500

    Remove redundancy for header count

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -4,25 +4,23 @@ function renderCheckoutHeader() {
   const total = calculateTotalQuantity();
   const checkoutHeader = document.querySelector(".checkout-header");
   checkoutHeader.innerHTML = `
-   <div class="checkout-header">
-      <div class="header-content">
-        <div class="checkout-header-left-section">
-          <a href="amazon.html">
-            <img class="amazon-logo" src="images/amazon-logo.png" />
-            <img
-              class="amazon-mobile-logo"
-              src="images/amazon-mobile-logo.png"
-            />
-          </a>
-        </div>
+    <div class="header-content">
+      <div class="checkout-header-left-section">
+        <a href="amazon.html">
+          <img class="amazon-logo" src="images/amazon-logo.png" />
+          <img
+            class="amazon-mobile-logo"
+            src="images/amazon-mobile-logo.png"
+          />
+        </a>
+      </div>
 
-        <div class="checkout-header-middle-section">
-          Checkout (<a class="return-to-home-link" href="amazon.html">${total === 0 ? "" : total}</a>)
-        </div>
+      <div class="checkout-header-middle-section">
+        Checkout (<a class="return-to-home-link" href="amazon.html">${total === 0 ? "" : total}</a>)
+      </div>
 
-        <div class="checkout-header-right-section">
-          <img src="images/icons/checkout-lock-icon.png" />
-        </div>
+      <div class="checkout-header-right-section">
+        <img src="images/icons/checkout-lock-icon.png" />
       </div>
     </div>
  `;
